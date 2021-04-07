ae9908aeac1197c05b25611899dfc9e72899c76b
Anusha2294 <Anusha.Reddy@syf.com>
2021-04-07 11:29:59 +0000

    Practice files

--- Test6.js
+++ Test6.js
@@ -1,26 +1,32 @@
+function demoAdd(a,b) {
+    return a+b
+}
 
-function demoAddA11(n)
-{
-    let (out = 0);
-    for (let i=0; i<n.length;i++)
-    {
-        out = out+n[i];
+let res = demoAdd(2,3);
+console.log(res);
+
+console.log(demoAdd(20,30));
+
+function demoAddAll(n) {
+    let out = 0;
+    for(let i=0;i<n.length;i++){
+        out=out+n[i];
     }
     return out;
 }
+
 let arr=[10,20,30,40,50];
-console.log(demoAddA11(arr));
-console.log(demoAddA11([10,20,30,40]));
+console.log(demoAddAll(arr));
+console.log(demoAddAll([10,20,30,40]));
 
-function demoAddA112(...n)
-{
-    let out =0;
-    for (let i=0; i<n.length;i++)
-    {
-        out= out+n[i];
+function demoAddAll2(...n) { //rest parameter : accepts any number of values
+    let out = 0;
+    for(let i=0;i<n.length;i++){
+        out=out+n[i];
     }
     return out;
 }
 
-console.log(demoAddA112(2,4,1,5,6));
-console.log(demoAddA112(...arr));
+console.log(demoAddAll2(2,4,1,5,6));
+
+console.log(demoAddAll2(...arr)); //spread parameter
