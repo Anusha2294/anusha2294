56f89252079d4b76f09a19b2e93e035d0ad9a3c8
Anusha2294 <Anusha.Reddy@syf.com>
2021-04-08 12:47:32 +0000

    Practice files 8th

---
+++ Test10.js
@@ -0,0 +1,35 @@
+//write a function to verify value if exist in an array
+
+let states = ["ts", "ap", "ka", "dl", "mh"];
+
+
+console.log(isValueExist(states, "dl"));
+console.log(isValueExist(states, "kl"));
+console.log(isValueExist(states, "ts"));
+
+function isValueExist(lstData, exp) 
+{
+    let isValueFound=false;
+    for (let i = 0; i < lstData.length; i++) 
+    {
+        if (lstData[i] == exp) 
+        {
+            isValueFound = true;
+            break; //exit from loop
+        
+        }
+    }
+    return isValueFound;
+}
+function isValueExist1(lstData, exp) 
+{
+
+    for (let i = 0; i < lstData.length; i++) 
+    {
+        if (lstData[i] == exp) 
+        {
+            return true;
+        }
+    }
+    return false;
+}
